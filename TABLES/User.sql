05523b21e4371aa167b83abd509cbb3c414987e1
Rodrigo Furlaneti <rodrigofurlaneti31@hotmail.com>
2023-10-18 07:55:43 -0300

    Create User.sql

---
+++ TABLES/User.sql
@@ -0,0 +1,24 @@
+USE [Donne]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[User](
+	[UserId] [int] IDENTITY(1,1) NOT NULL,
+	[UserName] [varchar](50) NULL,
+	[UserPassword] [varchar](50) NULL,
+	[ProfileId] [int] NULL,
+	[ProfileName] [varchar](50) NULL,
+	[Status] [bit] NULL,
+ CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED 
+(
+	[UserId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
