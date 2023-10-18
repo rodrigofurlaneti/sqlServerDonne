5c23b9cfbe67951bd3da3fc5b5078ee01d68e6a8
Rodrigo Furlaneti <rodrigofurlaneti31@hotmail.com>
2023-10-18 07:23:47 -0300

    Create Order.sql

---
+++ TABLES/Order.sql
@@ -0,0 +1,29 @@
+USE [Donne]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Order](
+	[OrderId] [int] IDENTITY(1,1) NOT NULL,
+	[CommandId] [int] NULL,
+	[ProductId] [int] NULL,
+	[ProductName] [varchar](150) NULL,
+	[SalePrice] [varchar](50) NULL,
+	[Amount] [int] NULL,
+	[TotalSalePrice] [varchar](50) NULL,
+	[DateInsert] [datetime] NULL,
+	[DateUpdate] [datetime] NULL,
+	[UserId] [int] NULL,
+	[UserName] [varchar](150) NULL,
+ CONSTRAINT [PK_Order] PRIMARY KEY CLUSTERED 
+(
+	[OrderId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
