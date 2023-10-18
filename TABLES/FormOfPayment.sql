1fa236d8e8f8775ddff326d7c8b034dec0ef8612
Rodrigo Furlaneti <rodrigofurlaneti31@hotmail.com>
2023-10-18 07:16:17 -0300

    Create FormOfPayment.sql

---
+++ TABLES/FormOfPayment.sql
@@ -0,0 +1,22 @@
+USE [Donne]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[FormOfPayment](
+	[FormOfPaymentId] [int] IDENTITY(1,1) NOT NULL,
+	[FormOfPaymentName] [varchar](50) NULL,
+	[DateInsert] [datetime] NULL,
+	[DateUpdate] [datetime] NULL,
+	[UserId] [int] NULL,
+	[UserName] [varchar](150) NULL,
+ CONSTRAINT [PK_FormOfPayment] PRIMARY KEY CLUSTERED 
+(
+	[FormOfPaymentId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
