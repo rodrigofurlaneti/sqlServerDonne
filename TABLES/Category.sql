62af01a779020440eda720105d4ccc68fd5d395f
Rodrigo Furlaneti <rodrigofurlaneti31@hotmail.com>
2023-10-18 06:59:46 -0300

    Create Category.sql

---
+++ TABLES/Category.sql
@@ -0,0 +1,22 @@
+USE [Donne]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Category](
+	[CategoryId] [int] IDENTITY(1,1) NOT NULL,
+	[CategoryName] [varchar](150) NULL,
+	[DateInsert] [datetime] NULL,
+	[DateUpdate] [datetime] NULL,
+	[UserId] [int] NULL,
+	[UserName] [varchar](150) NULL,
+ CONSTRAINT [PK_Category] PRIMARY KEY CLUSTERED 
+(
+	[CategoryId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
