c3a00b3fc7f79d8f240bf65f7ae7d232161b1866
Rodrigo Furlaneti <rodrigofurlaneti31@hotmail.com>
2023-10-18 07:49:22 -0300

    Create USP_UserDelete.sql

---
+++ PROCEDURE/User/USP_UserDelete.sql
@@ -0,0 +1,19 @@
+USE [Donne]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[USP_UserDelete] (@UserId INT)
+AS
+BEGIN
+	DELETE FROM [dbo].[User]
+		WHERE UserId = @UserId
+	SET NOCOUNT ON;
+END
+GO
+
+
