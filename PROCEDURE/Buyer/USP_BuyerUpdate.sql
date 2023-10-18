0bb98818e082dc124243c0344566b60bb3a51250
Rodrigo Furlaneti <rodrigofurlaneti31@hotmail.com>
2023-10-18 06:52:00 -0300

    Create USP_BuyerUpdate.sql

---
+++ PROCEDURE/Buyer/USP_BuyerUpdate.sql
@@ -0,0 +1,34 @@
+USE [Donne]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[USP_BuyerUpdate]	(@BuyerId INT,
+											@BuyerName VARCHAR(50),
+											@BuyerPhone VARCHAR(50),
+											@BuyerAddress VARCHAR(100),
+											@DateInsert DATETIME,
+											@DateUpdate DATETIME,
+											@UserId INT,
+											@UserName VARCHAR(150))
+
+AS
+BEGIN
+	UPDATE Buyer
+		SET BuyerName = @BuyerName,
+			BuyerPhone = @BuyerPhone,
+			BuyerAddress = @BuyerAddress,
+			DateInsert = @DateInsert,
+			DateUpdate = @DateUpdate,
+			UserId = @UserId,
+			UserName = @UserName
+				WHERE BuyerId = @BuyerId
+	SET NOCOUNT ON;
+END
+GO
+
+
