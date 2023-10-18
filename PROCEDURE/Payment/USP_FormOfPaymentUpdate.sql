d48de4f9cfa11d1830b8d800016f7efb1876f7e7
Rodrigo Furlaneti <rodrigofurlaneti31@hotmail.com>
2023-10-18 07:32:40 -0300

    Create USP_FormOfPaymentUpdate.sql

---
+++ PROCEDURE/Payment/USP_FormOfPaymentUpdate.sql
@@ -0,0 +1,29 @@
+USE [Donne]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+  CREATE PROCEDURE [dbo].[USP_FormOfPaymentUpdate] (@FormOfPaymentId INT,
+									@FormOfPaymentName VARCHAR(50),
+									@DateInsert DATETIME,
+									@DateUpdate DATETIME,
+									@UserId INT,
+									@UserName VARCHAR(150))
+	AS
+		BEGIN
+			UPDATE FormOfPayment
+			SET FormOfPaymentName = @FormOfPaymentName,
+				DateInsert = @DateInsert,
+				DateUpdate = @DateUpdate,
+				UserId = @UserId,
+				UserName = @UserName
+					WHERE FormOfPaymentId = @FormOfPaymentId
+			SET NOCOUNT ON;
+		END
+GO
+
+
