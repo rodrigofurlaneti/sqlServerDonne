dae7c68105da7be3352586824ade72c829cb365f
Rodrigo Furlaneti <rodrigofurlaneti31@hotmail.com>
2023-10-18 07:43:48 -0300

    Create USP_ProductGetById.sql

---
+++ PROCEDURE/Product/USP_ProductGetById.sql
@@ -0,0 +1,17 @@
+USE [Donne]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[USP_ProductGetById] (@ProductId INT)
+AS
+BEGIN
+	SELECT TOP 1 * FROM Product
+		WHERE ProductId = @ProductId;
+	SET NOCOUNT ON;
+END
+GO
